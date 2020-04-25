619e51d8c146cebca444f318078028803d850405
ReenaVerma <reena_verma@hotmail.com>
2020-04-25 19:11:18 +0100

    Loop through ingredients/state and pass as props

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,21 @@ import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
 // using curley braces as we want to return JSX
 
 const burger = (props) => {
+
+    //convert object into an array, so we can loop through it?
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            }); //create an Array with this [,]
+        });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
+            {/* <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="meat"/> */}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
