b4287d67f257ef7f9831944bed129c4548e0cd5c
ReenaVerma <reena_verma@hotmail.com>
2020-05-07 10:13:09 +0100

    Chapter 134: Disabled

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,7 @@ class BurgerBuilder extends Component {
             salad: 0,
             bacon: 0,
             cheese: 0,
-            meat: 1
+            meat: 0
         },
         totalPrice: 4
     }
@@ -42,15 +42,48 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) => {
+        // need to know first what is the old ingredient count and access for given type
+        const oldCount = this.state.ingredients[type];
+
+        if (oldCount <= 0) {
+            return;  // nothing happens if we reduce the ingredients
+        }
+
+        // calculate updated count
+        const updatedCount = oldCount - 1;
+        // State should be updated in an immutable way. So use spread operator first to create new object
+        const updatedIngredients = {...this.state.ingredients};
+        // Take new object and access the type. Then set the count value equal to updatedCount
+        updatedIngredients[type] = updatedCount;
+
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients});
 
     }
 
     render() {
+
+        const disableInfo = {
+            ...this.state.ingredients
+        };
+
+        for (let key in disableInfo) {
+            // true will be assigned if disabled
+            disableInfo[key] = disableInfo[key] <= 0;
+            console.log('disableInfo[key]', disableInfo[key]);
+        }
+        // {salad: true, meat: false...}
+
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disableInfo}
                 />
             </Aux>
         );
